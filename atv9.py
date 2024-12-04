4671127787beef39b8e4f2232206e9e717d69be7
Seu Nome <ferreiraashi@gmail.com>
2024-12-04 09:19:25 -0400

    ajustes na atividade 9

--- atv9.py
+++ atv9.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
 
 # Utilizar o comando ¨source venv/bin/activate.fish" para ativar o ambiente virtual e rodar o selenium (n baixei globalmente para não causar conflitos)
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
-driver.get("https://example.com")
+driver.get("https://the-internet.herokuapp.com/javascript_alerts")
 
-# Lidar com o alerta
-alert = Alert(driver)
-alert.accept()  # Para fechar o alerta
+button = driver.find_element(By.XPATH, "//button[text()='Click for JS Alert']")
+button.click()
+    
+alert = driver.switch_to.alert
+print(f"Texto do alerta: {alert.text}")
+alert.accept()
 
 driver.quit()
